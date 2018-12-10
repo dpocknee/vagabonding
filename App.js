9f813ac4b00c79bed9f338f468a517b5c57f921a
Lluran <aaron.boniface@gmail.com>
2018-12-10 11:20:50 +0000

    Configure Splash screen and inital landing (loading)page

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
 import * as firebase from 'firebase';
 import 'firebase/firestore';
@@ -10,13 +8,12 @@ firebase.initializeApp(firebaseConfig);
 const firestore = firebase.firestore();
 firestore.settings(settings);
 
-const loginFlow = createSwitchNavigator({
-  // add login components here - remember to include screen property
-});
+const loginFlow = createSwitchNavigator();
 
-const mainFlow = createStackNavigator({
-  // Add main app components here - remember to include screen property
-});
+// add login components here - remember to include screen property
+
+const mainFlow = createStackNavigator();
+// add login components here - remember to include screen property
 
 const appNavigation = createSwitchNavigator(
   {
