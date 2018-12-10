5e4f962644b3a21b103a686591b01b7317959a20
Joanna Cholewa <34255500+jcholewa@users.noreply.github.com>
2018-12-10 15:04:44 +0000

    Add name and username to sign up & user doc

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,39 +1,41 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet, Text, TextInput, View, Button,
-} from 'react-native';
-import * as firebase from 'firebase';
-import { CheckBox } from 'react-native-elements';
-import 'firebase/firestore';
+import React, { Component } from "react";
+import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import * as firebase from "firebase";
+import { CheckBox } from "react-native-elements";
+import "firebase/firestore";
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
   textInput: {
     height: 40,
-    width: '90%',
-    borderColor: 'gray',
+    width: "90%",
+    borderColor: "gray",
     borderWidth: 1,
-    marginTop: 8,
-  },
+    marginTop: 8
+  }
 });
 
 class SignUp extends Component {
   state = {
-    email: '',
-    password: '',
+    name: "",
+    username: "",
+    email: "",
+    password: "",
     errorMessage: null,
     museumsChecked: false,
     barsChecked: false,
-    restaurantsChecked: false,
+    restaurantsChecked: false
   };
 
   handleSignUp = () => {
     const {
+      name,
+      username,
       email,
       password,
       museumsChecked,
       barsChecked,
-      restaurantsChecked,
+      restaurantsChecked
     } = this.state;
 
     firebase
@@ -43,7 +45,7 @@ class SignUp extends Component {
         const { currentUser } = firebase.auth();
         firebase
           .firestore()
-          .collection('users')
+          .collection("users")
           .doc(currentUser.uid)
           .set({
             location: { latitude: 0, longitude: 0 },
@@ -51,12 +53,14 @@ class SignUp extends Component {
             museumsChecked,
             barsChecked,
             restaurantsChecked,
+            name,
+            username
           });
-        this.props.navigation.navigate('mainFlow');
+        this.props.navigation.navigate("mainFlow");
       })
-      .catch((err) => {
+      .catch(err => {
         this.setState({
-          errorMessage: err.message,
+          errorMessage: err.message
         });
       });
   };
@@ -64,16 +68,33 @@ class SignUp extends Component {
   render() {
     const {
       errorMessage,
+      name,
+      username,
       email,
       password,
       museumsChecked,
       barsChecked,
-      restaurantsChecked,
+      restaurantsChecked
     } = this.state;
+
     return (
       <View style={styles.container}>
         <Text>Sign Up</Text>
-        {errorMessage && <Text style={{ color: 'red' }}>{errorMessage}</Text>}
+        {errorMessage && <Text style={{ color: "red" }}>{errorMessage}</Text>}
+        <TextInput
+          placeholder="name"
+          autoCapitalize="words"
+          style={styles.textInput}
+          onChangeText={name => this.setState({ name })}
+          value={name}
+        />
+        <TextInput
+          placeholder="username"
+          autoCapitalize="words"
+          style={styles.textInput}
+          onChangeText={username => this.setState({ username })}
+          value={username}
+        />
         <TextInput
           placeholder="email"
           autoCapitalize="none"
@@ -103,14 +124,15 @@ class SignUp extends Component {
         <CheckBox
           title="Local restaurants"
           checked={restaurantsChecked}
-          onPress={() => this.setState({ restaurantsChecked: !restaurantsChecked })
+          onPress={() =>
+            this.setState({ restaurantsChecked: !restaurantsChecked })
           }
         />
         <Button title="Sign Up" onPress={this.handleSignUp} />
         <Button
           title="Already have an account? Login"
           onPress={() => {
-            this.props.navigation.navigate('LogIn');
+            this.props.navigation.navigate("LogIn");
           }}
         />
       </View>
