a549d7b2084bdf4a7672c278938d4a14847b4b2d
Lluran <aaron.boniface@gmail.com>
2018-12-10 09:13:54 +0000

    Created appnavigator

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {
-  createSwitchNavigator,
-  createStackNavigator,
-  createAppContainer,
-} from 'react-navigation';
+import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
 
 const loginFlow = createSwitchNavigator({});
 
 const mainFlow = createStackNavigator({});
 
-const appNavigation = createStackNavigator({
-  loginFlow,
-  mainFlow,
-});
+const appNavigation = createStackNavigator(
+  {
+    loginFlow,
+    mainFlow,
+  },
+  { initialRouteName: 'loginFlow' },
+);
 
 const App = createAppContainer(appNavigation);
 
