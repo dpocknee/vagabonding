4b38ff262743ea0e80a763ea7abbf20e53ea4ac5
Lluran <aaron.boniface@gmail.com>
2018-12-10 09:50:07 +0000

    Set firebase config

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import { firebaseConfig, settings } from './config';
+
+firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore();
+firestore.settings(settings);
 
 const loginFlow = createSwitchNavigator({});
 
