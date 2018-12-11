c6edc0ba5e46503fe7d671be7c71f4f2b44a8c26
dpocknee <dappy_iodo@hotmail.com>
2018-12-11 10:46:58 +0000

    attempt to commit sidebar and mapscreen.

--- components/MapScreen.js
+++ components/MapScreen.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import * as Expo from 'expo';
+import { Drawer, Button, Icon } from 'native-base';
+import Sidebar from './Sidebar';
+
+/* eslint no-underscore-dangle: 0 */
 
 export default class App extends Component {
   state = {
@@ -15,7 +19,6 @@ export default class App extends Component {
     const { status } = await Expo.Permissions.askAsync(Expo.Permissions.LOCATION);
     if (status !== 'granted') {
       const oldTrafford = (await Expo.Location.geocodeAsync('Sir Matt Busby Way'))[0];
-      // console.error('Location denied');
       this.setState({
         location: oldTrafford,
       });
@@ -27,22 +30,83 @@ export default class App extends Component {
     }
   };
 
+  closeDrawer = () => {
+    this.drawer._root.close();
+  };
+
+  openDrawer = () => {
+    this.drawer._root.open();
+  };
+
+  toggleDrawer = () => {
+    this.drawer._root.toggle();
+  };
+
   render() {
     const { location } = this.state;
     if (!location) {
       return <View />;
     }
+    const drawerStyles = {
+      drawer: {
+        backgroundColor: 'transparent',
+        height: 100,
+      },
+      main: {
+        paddingLeft: 3,
+        backgroundColor: 'transparent',
+      },
+      drawerOverlay: {
+        opacity: 0,
+      },
+      mainOverlay: {
+        opacity: 0,
+        backgroundColor: 'black',
+        shadowColor: '#000000',
+        shadowOpacity: 0.8,
+      },
+      // Check here for native-base drawer styles: https://github.com/root-two/react-native-drawer
+      // The native-base drawer component uses the same props as react-native-drawer.
+    };
     return (
-      <Expo.MapView
-        style={{ flex: 1 }}
-        provider={Expo.MapView.PROVIDER_GOOGLE}
-        initialRegion={{
-          latitude: location.coords.latitude,
-          longitude: location.coords.longitude,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'red',
         }}
-      />
+      >
+        <Drawer
+          type="overlay"
+          styles={drawerStyles}
+          side="top"
+          ref={(ref) => {
+            this.drawer = ref;
+          }}
+          content={(
+            <Sidebar
+              navigator={this.navigator}
+              allNav={this.allNav}
+              onPress={this.closeDrawer}
+              closer={this.closeDrawer}
+            />
+)}
+          onClose={() => this.closeDrawer()}
+        >
+          <Button iconLeft transparent onPress={() => this.toggleDrawer()} width={50}>
+            <Icon type="FontAwesome" name="bars" />
+          </Button>
+          <Expo.MapView
+            style={{ flex: 1 }}
+            provider={Expo.MapView.PROVIDER_GOOGLE}
+            initialRegion={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              latitudeDelta: 0.01,
+              longitudeDelta: 0.01,
+            }}
+          />
+        </Drawer>
+      </View>
     );
   }
 }
---
+++ components/Sidebar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Text } from 'react-native';
+import {
+  Container, Content, Button, Icon,
+} from 'native-base';
+import propTypes from 'prop-types';
+
+const Sidebar = (props) => {
+  const { closer, allNav } = props;
+  const menuTextStyle = { left: 5, color: 'darkblue' };
+  return (
+    <Container
+      style={{
+        backgroundColor: 'white',
+        opacity: 0.8,
+        width: 150,
+        flex: 0.5,
+      }}
+    >
+      <Content>
+        <Button
+          iconLeft
+          transparent
+          primary
+          style={{ height: 50, width: 50, left: 3 }}
+          onPress={() => closer()}
+        >
+          <Icon type="FontAwesome" name="bars" />
+        </Button>
+
+        <Button iconLeft transparent primary title="Map" onPress={() => allNav('Map')}>
+          <Icon type="FontAwesome" name="map" />
+          <Text style={menuTextStyle}>Map</Text>
+        </Button>
+        <Button iconLeft transparent primary title="Inbox" onPress={() => allNav('Inbox')}>
+          <Icon type="FontAwesome" name="envelope" />
+          <Text style={menuTextStyle}>Inbox</Text>
+        </Button>
+        <Button iconLeft transparent primary title="Logout" onPress={() => allNav('Logout')}>
+          <Icon type="FontAwesome" name="times" />
+          <Text style={menuTextStyle}>Logout</Text>
+        </Button>
+      </Content>
+    </Container>
+  );
+};
+
+Sidebar.propTypes = {
+  closer: propTypes.func.isRequired,
+  allNav: propTypes.func.isRequired,
+};
+
+export default Sidebar;
