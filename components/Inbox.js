59c68d63bd58d071f972641aae7109183fc01f23
Lluran <aaron.boniface@gmail.com>
2018-12-14 12:32:54 +0000

    New attempt at merge

--- components/Inbox.js
+++ components/Inbox.js
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
-import { View, ScrollView, Text, Button } from "react-native";
-import { getTheme } from "react-native-material-kit";
-import firebase from "firebase";
+import React, { Component } from 'react';
+import {
+  View, ScrollView, Text, Button,
+} from 'react-native';
+import { getTheme } from 'react-native-material-kit';
+import firebase from 'firebase';
 
-const {
-  getChatPartnerNames,
-  chatsRef
-} = require("../Functionality/chatFunctions");
+const { getChatPartnerNames, chatsRef } = require('../Functionality/chatFunctions');
 
 // console.log('currentUserSTUFF: ', currentUser, currentUserID);
 const theme = getTheme();
@@ -15,55 +14,49 @@ class Inbox extends Component {
   //* *********NEEDS CURRENT USERID AS PROP*************** */
   state = {
     chats: [],
-    loading: true
+    loading: true,
   };
 
   componentDidMount() {
     let currentUserID;
-    firebase.auth().onAuthStateChanged(user => {
+    firebase.auth().onAuthStateChanged((user) => {
       currentUserID = user.uid;
-      const allUserChats = chatsRef.where(
-        "usersArr",
-        "array-contains",
-        `${currentUserID}`
-      );
-      allUserChats.onSnapshot(querySnapshot => {
-        querySnapshot.docChanges().forEach(change => {
-          if (change.type === "added") {
+      const allUserChats = chatsRef.where('usersArr', 'array-contains', `${currentUserID}`);
+      allUserChats.onSnapshot((querySnapshot) => {
+        querySnapshot.docChanges().forEach((change) => {
+          if (change.type === 'added') {
             const chatObj = {};
             change.doc.data().usersArr[0] === currentUserID
               ? (chatObj.otherUser = change.doc.data().usersArr[1])
               : (chatObj.otherUser = change.doc.data().usersArr[0]);
             chatObj.messages = change.doc.data().messages;
-            getChatPartnerNames(chatObj.otherUser).then(chatPartnerObj => {
+            getChatPartnerNames(chatObj.otherUser).then((chatPartnerObj) => {
               const compObj = {
                 ...chatObj,
                 otherUserUsername: chatPartnerObj.username,
-                otherUserName: chatPartnerObj.name
+                otherUserName: chatPartnerObj.name,
               };
               this.setState(previousState => ({
-                chats: [...previousState.chats, compObj]
+                chats: [...previousState.chats, compObj],
               }));
             });
           }
-          if (change.type === "modified") {
+          if (change.type === 'modified') {
             const chatObj = {};
             change.doc.data().usersArr[0] === currentUserID
               ? (chatObj.otherUser = change.doc.data().usersArr[1])
               : (chatObj.otherUser = change.doc.data().usersArr[0]);
             chatObj.messages = change.doc.data().messages;
-            getChatPartnerNames(chatObj.otherUser).then(chatPartnerObj => {
+            getChatPartnerNames(chatObj.otherUser).then((chatPartnerObj) => {
               const compObj = {
                 ...chatObj,
                 otherUserUsername: chatPartnerObj.username,
-                otherUserName: chatPartnerObj.name
+                otherUserName: chatPartnerObj.name,
               };
               const currentChats = [...this.state.chats];
-              const oldChats = currentChats.filter(chatObj => {
-                return chatObj.otherUser !== compObj.otherUser;
-              });
+              const oldChats = currentChats.filter(chatObj => chatObj.otherUser !== compObj.otherUser);
               this.setState({
-                chats: [...oldChats, compObj]
+                chats: [...oldChats, compObj],
               });
             });
             /*
@@ -84,13 +77,10 @@ class Inbox extends Component {
         {chats.map((chat, index) => (
           <View style={theme.cardStyle} key={index}>
             <Text style={theme.cardActionStyle}>
-              {`Conversation with: ${chat.otherUserName} (${
-                chat.otherUserUsername
-              })`}{" "}
+              {`Conversation with: ${chat.otherUserName} (${chat.otherUserUsername})`}
+              {' '}
             </Text>
-            <Text style={theme.cardContentStyle}>{`${
-              chat.messages.length
-            } messages`}</Text>
+            <Text style={theme.cardContentStyle}>{`${chat.messages.length} messages`}</Text>
             {/* <Button title="chat" onPress={() => navigation.navigate('Chat')} /> ******NEEDS CURRENT USERNAME, ID and OTHERUSERID AS PROPS */}
           </View>
         ))}
