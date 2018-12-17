7a50ca699998b893f914f83bf7b6a11ad45590c0
Lluran <aaron.boniface@gmail.com>
2018-12-17 11:00:36 +0000

    Add unsubscribe to Inbox onAuthStateChange function to fix app-breaking logout error

--- components/Inbox.js
+++ components/Inbox.js
@@ -16,58 +16,63 @@ class Inbox extends Component {
   };
 
   componentDidMount() {
-    let currentUserID;
-    firebase.auth().onAuthStateChanged((user) => {
-      currentUserID = user.uid;
-      getCurrentUserInfo(currentUserID).then((currentUserInfo) => {
-        this.setState({
-          currentUserID,
-          currentUsername: currentUserInfo.username,
-        });
-      });
-      const allUserChats = chatsRef.where('usersArr', 'array-contains', `${currentUserID}`);
-      allUserChats.onSnapshot((querySnapshot) => {
-        querySnapshot.docChanges().forEach((change) => {
-          if (change.type === 'added') {
-            const chatObj = {};
-            change.doc.data().usersArr[0] === currentUserID
-              ? (chatObj.otherUser = change.doc.data().usersArr[1])
-              : (chatObj.otherUser = change.doc.data().usersArr[0]);
-            chatObj.messages = change.doc.data().messages;
-            getChatPartnerNames(chatObj.otherUser).then((chatPartnerObj) => {
-              const compObj = {
-                ...chatObj,
-                otherUserUsername: chatPartnerObj.username,
-                otherUserName: chatPartnerObj.name,
-              };
-              this.setState(previousState => ({
-                chats: [...previousState.chats, compObj],
-              }));
-            });
-          }
-          if (change.type === 'modified') {
-            const chatObj = {};
-            change.doc.data().usersArr[0] === currentUserID
-              ? (chatObj.otherUser = change.doc.data().usersArr[1])
-              : (chatObj.otherUser = change.doc.data().usersArr[0]);
-            chatObj.messages = change.doc.data().messages;
-            getChatPartnerNames(chatObj.otherUser).then((chatPartnerObj) => {
-              const compObj = {
-                ...chatObj,
-                otherUserUsername: chatPartnerObj.username,
-                otherUserName: chatPartnerObj.name,
-              };
-              const currentChats = [...this.state.chats];
-              const oldChats = currentChats.filter(
-                chatObject => chatObject.otherUser !== compObj.otherUser,
-              );
-              this.setState({
-                chats: [...oldChats, compObj],
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      const userID = user.uid;
+      getCurrentUserInfo(userID).then((currentUserInfo) => {
+        this.setState(
+          {
+            currentUserID: userID,
+            currentUsername: currentUserInfo.username,
+          },
+          () => {
+            const { currentUserID } = this.state;
+            const allUserChats = chatsRef.where('usersArr', 'array-contains', `${currentUserID}`);
+            allUserChats.onSnapshot((querySnapshot) => {
+              querySnapshot.docChanges().forEach((change) => {
+                if (change.type === 'added') {
+                  const chatObj = {};
+                  change.doc.data().usersArr[0] === currentUserID
+                    ? (chatObj.otherUser = change.doc.data().usersArr[1])
+                    : (chatObj.otherUser = change.doc.data().usersArr[0]);
+                  chatObj.messages = change.doc.data().messages;
+                  getChatPartnerNames(chatObj.otherUser).then((chatPartnerObj) => {
+                    const compObj = {
+                      ...chatObj,
+                      otherUserUsername: chatPartnerObj.username,
+                      otherUserName: chatPartnerObj.name,
+                    };
+                    this.setState(previousState => ({
+                      chats: [...previousState.chats, compObj],
+                    }));
+                  });
+                }
+                if (change.type === 'modified') {
+                  const chatObj = {};
+                  change.doc.data().usersArr[0] === currentUserID
+                    ? (chatObj.otherUser = change.doc.data().usersArr[1])
+                    : (chatObj.otherUser = change.doc.data().usersArr[0]);
+                  chatObj.messages = change.doc.data().messages;
+                  getChatPartnerNames(chatObj.otherUser).then((chatPartnerObj) => {
+                    const compObj = {
+                      ...chatObj,
+                      otherUserUsername: chatPartnerObj.username,
+                      otherUserName: chatPartnerObj.name,
+                    };
+                    const currentChats = [...this.state.chats];
+                    const oldChats = currentChats.filter(
+                      chatObject => chatObject.otherUser !== compObj.otherUser,
+                    );
+                    this.setState({
+                      chats: [...oldChats, compObj],
+                    });
+                  });
+                }
               });
             });
-          }
-        });
+          },
+        );
       });
+      unsubscribe();
     });
   }
 
