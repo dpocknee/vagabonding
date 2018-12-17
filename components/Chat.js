f7b2cc6f7255fd28d14550c1a3f553c21c837411
Joanna Cholewa <34255500+jcholewa@users.noreply.github.com>
2018-12-17 12:58:52 +0000

    Error handling for chat functions

--- components/Chat.js
+++ components/Chat.js
@@ -1,41 +1,41 @@
-import React, { Component } from 'react';
-import { GiftedChat } from 'react-native-gifted-chat';
-import ErrorComponent from './ErrorComponent';
+import React, { Component } from "react";
+import { GiftedChat } from "react-native-gifted-chat";
+import ErrorComponent from "./ErrorComponent";
 
 const {
   getPreviousMessages,
   sendMessage,
-  chatsRef,
-} = require('../Functionality/chatFunctions');
+  chatsRef
+} = require("../Functionality/chatFunctions");
 
 class Chat extends Component {
   state = {
     messages: [],
-    doc: '',
-    errorMessage: null,
+    doc: "",
+    errorMessage: null
   };
 
   componentWillMount() {
     const { currentUserID, selectedUserID } = this.props;
     getPreviousMessages(currentUserID, selectedUserID)
-      .then((messageObj) => {
-        if (typeof messageObj !== 'object') {
+      .then(messageObj => {
+        if (typeof messageObj !== "object") {
           this.setState({
-            errorMessage: messageObj,
+            errorMessage: messageObj
           });
         } else {
-          chatsRef.doc(messageObj.doc).onSnapshot((doc) => {
+          chatsRef.doc(messageObj.doc).onSnapshot(doc => {
             const messages = doc.data().messages.reverse();
             this.setState({
               doc: messageObj.doc,
-              messages,
+              messages
             });
           });
         }
       })
-      .catch((err) => {
+      .catch(err => {
         this.setState({
-          errorMessage: err.message,
+          errorMessage: err.message
         });
       });
   }
