69bcaecce7dceee3acdfeca802e6c61aab28cb8b
hughp23 <hughpaul14@gmail.com>
2018-12-17 17:13:42 +0000

    fixing loading page styling

--- components/Users.js
+++ components/Users.js
@@ -4,6 +4,7 @@ import {
 } from 'react-native';
 import { getTheme } from 'react-native-material-kit';
 import PropTypes from 'prop-types';
+import PTRView from 'react-native-pull-to-refresh';
 import Loading from './Loading';
 
 /* eslint react/require-default-props: 0 */
@@ -12,31 +13,43 @@ import Loading from './Loading';
 const theme = getTheme();
 
 const Users = (props) => {
-  const { users, onSelectUser, currentUser } = props;
+  const {
+    users, onSelectUser, currentUser, navigation,
+  } = props;
+  const _refresh = () => {
+    const refreshPromise = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 2000);
+    });
+    refreshPromise.then(() => navigation.push('Map'));
+  };
   if (!users) {
     return <Loading />;
   }
   return (
-    <ScrollView>
-      {users.map(
-        user => user[0] !== currentUser.uid && (
-        <TouchableOpacity
-          onPress={() => onSelectUser(user)}
-          style={theme.cardStyle}
-          key={user[1].name}
-        >
-          <Text style={theme.cardActionStyle}>
-            {user[1].name}
-            {' '}
-          </Text>
-          <Text style={theme.cardContentStyle}>
-            {user[1].distance === 0 ? 'Less Than 100' : user[1].distance}
+    <PTRView onRefresh={_refresh}>
+      <ScrollView>
+        {users.map(
+          user => user[0] !== currentUser.uid && (
+          <TouchableOpacity
+            onPress={() => onSelectUser(user)}
+            style={theme.cardStyle}
+            key={user[1].name}
+          >
+            <Text style={theme.cardActionStyle}>
+              {user[1].name}
+              {' '}
+            </Text>
+            <Text style={theme.cardContentStyle}>
+              {user[1].distance === 0 ? 'Less Than 100' : user[1].distance}
 m away
-          </Text>
-        </TouchableOpacity>
-        ),
-      )}
-    </ScrollView>
+            </Text>
+          </TouchableOpacity>
+          ),
+        )}
+      </ScrollView>
+    </PTRView>
   );
 };
 
