cb5ea5a96e1b435b9163bdf43a405b566ee7ef77
Claire McNally <clairemcnally@Claires-MacBook-Pro.local>
2018-12-18 11:32:40 +0000

    profile

--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-import { Button, Icon } from "native-base";
-import PropTypes from "prop-types";
-import MenuWrapper from "./MenuWrapper";
-import profileStyles from "../styles/Profile.styles";
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo';
+import { Button, Icon } from 'native-base';
+import PropTypes from 'prop-types';
+import MenuWrapper from './MenuWrapper';
+import profileStyles from '../styles/Profile.styles';
 
 export default class Profile extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -12,80 +13,95 @@ export default class Profile extends Component {
         iconLeft
         transparent
         onPress={() => {
-          navigation.getParam("drawerStatus")();
+          navigation.getParam('drawerStatus')();
         }}
         width={50}
       >
         <Icon type="FontAwesome" name="bars" />
       </Button>
     ),
-    title: "Profile Page"
+    title: 'Profile Page',
   });
 
   render() {
     const { navigation } = this.props;
-    const userId = this.props.navigation.getParam("selectedUser")[0];
-    const userInfo = this.props.navigation.getParam("selectedUser")[1];
-    const currentUser = this.props.navigation.getParam("currentUser");
-    const nearbyUsers = this.props.navigation.getParam("nearbyUsers");
+    const userId = this.props.navigation.getParam('selectedUser')[0];
+    const userInfo = this.props.navigation.getParam('selectedUser')[1];
+    const currentUser = this.props.navigation.getParam('currentUser');
+    const nearbyUsers = this.props.navigation.getParam('nearbyUsers');
     const currentUserInfo = nearbyUsers.filter(
-      user => user[0] === currentUser.uid
+      user => user[0] === currentUser.uid,
     );
 
     const currentUsername = currentUserInfo[0][1].username;
     const interests = userInfo.interests
       ? Object.keys(userInfo.interests).reduce((outputArray, interest) => {
-          if (userInfo.interests[interest]) outputArray.push(interest);
-          return outputArray;
-        }, [])
+        if (userInfo.interests[interest]) outputArray.push(interest);
+        return outputArray;
+      }, [])
       : [];
-    const validInterests =
-      interests.length > 0 ? interests.join(" / ") : "No interests given!";
+    const validInterests = interests.length > 0 ? interests.join(' / ') : 'No interests given!';
     return (
-      <View style={{ flex: 1 }}>
-        <MenuWrapper navigation={navigation}>
-          <>
-            <View style={profileStyles.profileText}>
-              <Icon
-                type="FontAwesome"
-                name="user-circle"
-                style={{ fontSize: 40 }}
-              />
-              <Text style={profileStyles.username}>{userInfo.username}</Text>
-              <Text style={profileStyles.info}>
-                Real name:
-                {userInfo.name}
-              </Text>
-              <Text style={profileStyles.info}>
-                Interests: {validInterests}
-              </Text>
-            </View>
-            <View style={profileStyles.chat}>
-              <TouchableOpacity
-                style={profileStyles.button}
-                onPress={() => {
-                  this.props.navigation.push("Chat", {
-                    currentUserID: currentUser.uid,
-                    currentUsername,
-                    selectedUserID: userId,
-                    selectedUserUsername: userInfo.username,
-                    selectedUsername: userInfo.name
-                  });
-                }}
-              >
-                <Text>
-                  Chat with
+      <View style={profileStyles.wholePage}>
+        <LinearGradient
+          colors={['rgba(225,225,225,225)', 'transparent']}
+          style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            height: 300,
+          }}
+        >
+          <MenuWrapper navigation={navigation}>
+            <>
+              <View style={profileStyles.profileBox}>
+                <Icon
+                  type="FontAwesome"
+                  name="user-circle"
+                  style={{ fontSize: 40 }}
+                />
+
+                <Text style={profileStyles.username}>{userInfo.username}</Text>
+              </View>
+              <View>
+                <Text style={profileStyles.info}>
+                  Real name:
                   {userInfo.name}
                 </Text>
-              </TouchableOpacity>
-            </View>
-          </>
-        </MenuWrapper>
+                <Text style={profileStyles.info}>
+                  Interests:
+                  {' '}
+                  {validInterests}
+                </Text>
+              </View>
+              <View style={profileStyles.chat}>
+                <TouchableOpacity
+                  style={profileStyles.button}
+                  onPress={() => {
+                    this.props.navigation.push('Chat', {
+                      currentUserID: currentUser.uid,
+                      currentUsername,
+                      selectedUserID: userId,
+                      selectedUserUsername: userInfo.username,
+                      selectedUsername: userInfo.name,
+                    });
+                  }}
+                >
+                  <Text>
+Chat with
+                    {userInfo.name}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </>
+          </MenuWrapper>
+        </LinearGradient>
       </View>
     );
   }
 }
 
 Profile.propTypes = {
-  navigation: PropTypes.object.isRequired
+  navigation: PropTypes.object.isRequired,
 };
--- styles/Profile.styles.js
+++ styles/Profile.styles.js
@@ -1,30 +1,40 @@
-import { getCenter } from "geolib";
+import { getCenter } from 'geolib';
 
 const profileStyles = {
-  profileText: {
-    paddingTop: 100,
-    paddingLeft: 50
+  wholePage: {
+    flex: 1,
+    backgroundColor: '#1D9FBF',
+  },
+  profileBox: {
+    marginTop: 30,
+    paddingTop: 50,
+    paddingLeft: 50,
+    // backgroundColor: "#FFFFFF",
+    width: '100%',
+    padding: 30,
   },
   username: {
-    fontWeight: "bold",
-    fontSize: 30
+    fontWeight: 'bold',
+    fontSize: 30,
+    paddingTop: 10,
   },
   info: {
-    fontWeight: "bold",
-    fontSize: 15
+    fontWeight: 'bold',
+    fontSize: 15,
+    paddingTop: 10,
   },
   chat: {
     flex: 1,
     width: 200,
     // alignItems: "center",
     paddingLeft: 40,
-    paddingTop: 20
+    paddingTop: 20,
   },
   button: {
     // alignItems: "center",
-    backgroundColor: "#F56463",
-    padding: 10
-  }
+    backgroundColor: '#F56463',
+    padding: 10,
+  },
 };
 
 export default profileStyles;
