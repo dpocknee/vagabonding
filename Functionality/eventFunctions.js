78fa6c413bbf81eef12227e0329057e7209e3350
Lluran <aaron.boniface@gmail.com>
2018-12-19 13:10:35 +0000

    Added ability to fetch nearbyEvents

--- Functionality/eventFunctions.js
+++ Functionality/eventFunctions.js
@@ -1,10 +1,12 @@
 import * as firebase from 'firebase';
 import 'firebase/firestore';
 import * as Expo from 'expo';
+import { isPointInCircle } from 'geolib';
 
 const { firestore } = require('../config');
 
 const eventsRef = firestore.collection('events');
+const usersRef = firestore.collection('users');
 
 // Function takes an object of event info
 // Creates a new object formatted for firestore storage
@@ -14,11 +16,7 @@ const getEventCoords = async locationString => Expo.Location.geocodeAsync(locati
 
 const addEvent = async (eventObj) => {
   const {
-    eventLocation,
-    eventName,
-    eventDescription,
-    datetime,
-    currentUserUID,
+    eventLocation, eventName, eventDescription, datetime, currentUserUID,
   } = eventObj;
   return getEventCoords(eventLocation).then((locationObj) => {
     const { latitude, longitude } = locationObj[0];
@@ -34,4 +32,30 @@ const addEvent = async (eventObj) => {
   });
 };
 
-export { addEvent, getEventCoords };
+const getNearbyEvents = async () => firebase.auth().onAuthStateChanged((currentUser) => {
+  const { uid } = currentUser;
+  return usersRef
+    .doc(uid)
+    .get()
+    .then(snapshot => Promise.all([eventsRef.get(), snapshot.data()]).then(([querySnapshot, userObj]) => {
+      const eventsData = [];
+      querySnapshot.forEach((doc) => {
+        eventsData.push({ id: doc.id, info: doc.data() });
+      });
+      const { location, radius } = userObj;
+      return Promise.all([
+        eventsData.filter((eventObj) => {
+          if (isPointInCircle(eventObj.info.location, location, radius)) {
+            return eventObj;
+          }
+        }),
+      ]).then(([nearbyEvents]) => {
+        console.log(nearbyEvents);
+      });
+    }));
+});
+// get all events
+// filter events - check if event.location = isPointinCircle
+// return filtered events (promise)
+
+export { addEvent, getEventCoords, getNearbyEvents };
--- components/NearbyEvents.js
+++ components/NearbyEvents.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { getNearbyEvents } from '../Functionality/eventFunctions';
 
 class NearbyEvents extends Component {
   state = {
     events: [],
   };
 
+  componentDidMount() {
+    // goGetNearbyEvents.then(nearbyEvents => {})
+    getNearbyEvents();
+    // set those events in state
+  }
+
   render() {
     const { events } = this.state;
     return (
       <View>
+        {events.map(eventObj => eventObj)}
         <Text>No nearby events</Text>
       </View>
     );
