28fb5980eff37375ff6786f996283f54b3a3c8c8
Kurtis Angell <KurtisBlow@MacBook-Pro.local>
2018-12-20 15:19:46 +0000

    menu wrapper on eventInfo

--- components/EventInfo.js
+++ components/EventInfo.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { LinearGradient } from 'expo';
-import { Button } from 'react-native-elements';
+// import { Button } from 'react-native-elements';
 import * as firebase from 'firebase';
+import { Button, Icon } from 'native-base';
 import profileStyles from '../styles/Profile.styles';
 import { getGuestNames, joinEvent } from '../Functionality/eventFunctions';
+import MenuWrapper from './MenuWrapper';
+import { titleStyles, iconStyles } from '../styles/Hamburger.styles';
 
 class EventInfo extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    headerLeft: (
+      <Button
+        iconLeft
+        transparent
+        onPress={() => {
+          navigation.getParam('buttonChange')();
+        }}
+        width={50}
+      >
+        <Icon type="FontAwesome" name="bars" style={iconStyles} />
+      </Button>
+    ),
+    title: 'eventInfo',
+    ...titleStyles,
+  });
+
   state = {
     event: {
       guestNames: [],
@@ -18,6 +38,9 @@ class EventInfo extends Component {
   };
 
   componentDidMount() {
+    const { navigation } = this.props;
+    navigation.setParams({ buttonChange: this.buttonChange });
+
     const eventInfo = this.props.navigation.getParam('eventObj');
     const dateArray = new Date(eventInfo.info.datetime).toString().split(' ');
     eventInfo.date = [...dateArray].splice(0, 4).join(' ');
@@ -46,56 +69,70 @@ class EventInfo extends Component {
     });
   }
 
+  buttonChange = () => {
+    this.setState((state) => {
+      const buttonClick = !state.button;
+      return { button: buttonClick };
+    });
+  };
+
   render() {
     const { event, user } = this.state;
+    const { navigation } = this.props;
     return (
       <View style={profileStyles.wholePage}>
-        <LinearGradient
-          colors={['rgba(225,225,225,225)', 'transparent']}
-          style={{
-            position: 'absolute',
-            left: 0,
-            right: 0,
-            top: 0,
-            // height: 300
-          }}
+        <MenuWrapper
+          navigation={navigation}
+          currentPage="eventInfo"
+          buttonState={this.state.button}
         >
-          <Text style={profileStyles.name}>{event.info.eventName}</Text>
-          <View style={profileStyles.info}>
-            <Text>
-              <Text style={profileStyles.catInfo}>Where:</Text>
-              {event.info.eventLocation}
-            </Text>
-            <Text>
-              <Text style={profileStyles.catInfo}>When:</Text>
-              {`${event.date} at ${event.time}`}
-            </Text>
-            <Text>
-              <Text style={profileStyles.catInfo}>What:</Text>
-              {event.info.eventDescription}
-            </Text>
-          </View>
-          <View style={profileStyles.info}>
-            <Text style={profileStyles.catInfo}>Going:</Text>
-            {event.guestNames.map((guest, index) => (
-              <Text key={event.info.guests[index] || Math.random() * 100}>{guest}</Text>
-            ))}
-          </View>
-          {!user && (
-            <Button
-              title="Join event"
-              onPress={() => {
-                joinEvent(event.id, (err, newName) => {
-                  const array = [...event.guestNames, newName];
-                  this.setState(prevState => ({
-                    event: { ...prevState.event, guestNames: array },
-                    user: true,
-                  }));
-                });
-              }}
-            />
-          )}
-        </LinearGradient>
+          <LinearGradient
+            colors={['rgba(225,225,225,225)', 'transparent']}
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              top: 0,
+              // height: 300
+            }}
+          >
+            <Text style={profileStyles.name}>{event.info.eventName}</Text>
+            <View style={profileStyles.info}>
+              <Text>
+                <Text style={profileStyles.catInfo}>Where:</Text>
+                {event.info.eventLocation}
+              </Text>
+              <Text>
+                <Text style={profileStyles.catInfo}>When:</Text>
+                {`${event.date} at ${event.time}`}
+              </Text>
+              <Text>
+                <Text style={profileStyles.catInfo}>What:</Text>
+                {event.info.eventDescription}
+              </Text>
+            </View>
+            <View style={profileStyles.info}>
+              <Text style={profileStyles.catInfo}>Going:</Text>
+              {event.guestNames.map((guest, index) => (
+                <Text key={event.info.guests[index] || Math.random() * 100}>{guest}</Text>
+              ))}
+            </View>
+            {!user && (
+              <Button
+                title="Join event"
+                onPress={() => {
+                  joinEvent(event.id, (err, newName) => {
+                    const array = [...event.guestNames, newName];
+                    this.setState(prevState => ({
+                      event: { ...prevState.event, guestNames: array },
+                      user: true,
+                    }));
+                  });
+                }}
+              />
+            )}
+          </LinearGradient>
+        </MenuWrapper>
       </View>
     );
   }
